738ae9f430142c50b41cd9487d609cc1e1af2c81
mberezet <solusrex@hotmail.com>
2019-03-04 01:07:23 -0500

    mem_0rder tsts

--- l_atomics_tst.cpp
+++ l_atomics_tst.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 using namespace std ;
 
@@ -42,12 +43,71 @@ void tst_lkspin()
   t4.join() ;
 }
 
+// acq-rel semantics
+atomic<int> semaf {0} ;
+int sh_var {0} ;
+
+void rShare() {
+  while( ! semaf.fetch_sub(1, std::memory_order_acquire) > 0)
+       this_thread::sleep_for(10000ms) ;
+
+  cerr << "shared = " << sh_var << endl ;
+  cerr <<  endl ;
+}
+
+void wShare() {
+  sh_var = 2019 ;
+  semaf.store(2, std::memory_order_release) ;
+}
+
+void tst_acqrel() {
+ thread tt0(wShare) ;
+
+ thread tt1(rShare) ;
+ thread tt2(rShare) ;
+ thread tt3(rShare) ;
+ thread tt4(rShare) ;
+ thread tt5(rShare) ;
+
+ tt0.join() ;
+
+ tt1.join() ;
+ tt2.join() ;
+ tt3.join() ;
+ tt4.join() ;
+ tt5.join() ;
+
+ cerr << "semaf = " << semaf << endl ;
+}
+
+// relaxed semantics
+atomic<int> refcnt {0};
+std::vector<thread> tpool;
+
+void inccnt() {
+  refcnt.fetch_add(1, std::memory_order_relaxed) ;
+}
+
+void tst_relaxed() {
+  for (int i=0; i<10; ++i)
+      tpool.emplace_back(inccnt) ; 
+
+  for (auto& t : tpool)
+      t.join() ;
+
+  cerr << "refcnt= " << refcnt << endl ;
+}
+
 ///////////////////////////////////////
 // driver
 // ////////////////////////////////////
 int main(int ac, char** av)
 {
-   tst_lkspin() ;
+
+   //tst_lkspin() ;
+   //tst_acqrel() ;
+   tst_relaxed() ;
+
 }
 
 
