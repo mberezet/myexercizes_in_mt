995ab3a56d52425efba100f35635377ed392b910
mberezet <solusrex@hotmail.com>
2019-03-19 15:33:35 -0400

    mutexies explanations added

--- muxes.cpp
+++ muxes.cpp
@@ -5,6 +5,16 @@
 
 using namespace std ;
 
+////////////////////////////////////////////////
+//  Ex 1:
+//  To illustrate how std::unique_lock prevents
+//     deadlock. Note that if i just lock regular mx or
+//     lock_guard - whatever - in t_main(), main() would
+//     have inevitable dead-lock, as it calls t1,t2 executor
+//     params in reverse order to each other !
+//     But mx's are not locked initially, and std::lock()
+//     is an ATOMIX ops --- so it 'collpases' 2 mx in 'one'
+//
 struct elem_t {
   int x{9} ;
   mutex m ;
@@ -13,10 +23,8 @@ struct elem_t {
 ///////////////////////////////////////////////////
 void t_main(elem_t& e1, elem_t& e2) {
   unique_lock<mutex> lo_1(e1.m, defer_lock) ;
-  this_thread::sleep_for(chrono::milliseconds(1)) ; 
-
+  this_thread::sleep_for(chrono::milliseconds(4)) ; 
   unique_lock<mutex> lo_2(e2.m, defer_lock) ;
-  this_thread::sleep_for(chrono::milliseconds(1)) ; 
 
   std::lock(lo_1, lo_2) ;    // this is ATOMIC lock of both locks !!!
   ++e1.x ;   ++e2.x ;
@@ -29,7 +37,7 @@ int main() {
   elem_t e_prev ;
 
   // old way ... NB! pass std::ref 
-  //             Passing as val will generate cpmpile error !
+  //             Passing as val will generate compile error !
   //thread t1( t_main, std::ref(e_nxt), std::ref(e_prev) ) ;
   //thread t2( t_main, std::ref(e_prev), std::ref(e_nxt) ) ;
 
