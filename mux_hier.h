cb32c0b5f4da883ee281aa94d09044bf1acd494d
mike B <mberezet@gmail.com>
2021-05-20 22:54:37 -0400

    vyravnivanie..

--- mux_hier.h
+++ mux_hier.h
@@ -17,11 +17,11 @@
 //               Thus order of the locking is enforced.
 //
 //               Each mx will update thread's currently held hier after lock (if validated)
-//                    and resets that tread's  prev held hier after unlock. 
+//               and resets that tread's  prev held hier after unlock. 
 //               Low-cost, since mx can be held by only one th at a time.
 //
 //  Cost: local_trhread mem access(hashing+indirection)  + usual cost of std::mutex usage + exception
-//        ( negligible if not invoked, significant if invoked)             
+//              ( negligible if not invoked, significant if invoked)             
 //
 //  NOTE: if u need to lock several locks SIMULT and want a protection from
 //        possible dedalok, use std::lock or std::unique_lock ! this
@@ -30,66 +30,65 @@
 //
 //  Our mux_hier needed when locking is TEMPORALY separated.                   
 
-
-class mux_hier {
-
+class mux_hier
+{
 public:
   constexpr static unsigned long MAXHIER = 2048 ;        // max. number of possible hier levels
 				
-	//     public std compatible interface (but we dont derive -- std classes are finals)
-	explicit mux_hier(unsigned long hier) : my_hier_{hier}, th_hilevel_prev_{0}
+  //     public std compatible interface (but we dont derive -- std classes are finals)
+  explicit mux_hier(unsigned long hier) : my_hier_{hier}, th_hilevel_prev_{0}
   { }
 
-	//  to follow std::mutex invariants
-	mux_hier()                           = delete ;
-	mux_hier(const mux_hier&)            = delete ;
-	mux_hier& operator=(const mux_hier&) = delete ;
+  //  to follow std::mutex invariants
+  mux_hier()                           = delete ;
+  mux_hier(const mux_hier&)            = delete ;
+  mux_hier& operator=(const mux_hier&) = delete ;
 
-	bool try_lock()
- 	{
-		validate_hier() ;
-		if (! mx_.try_lock())
-          return false ;
+  bool try_lock()
+  {
+     validate_hier() ;
+     if (! mx_.try_lock())
+        return false ;
 
-    set_th_hier() ;
-    return true ;
-	}
+     set_th_hier() ;
+     return true ;
+  }
 
   void lock()
- 	{
-		validate_hier() ;
-		mx_.lock() ;
+  {
+    validate_hier() ;
+    mx_.lock() ;
     set_th_hier() ;
-	}
+  }
 
-	void unlock() {
-		mx_.unlock() ;			
-		th_hilevel_curr = th_hilevel_prev_ ;
-	}
+  void unlock()
+  {
+    mx_.unlock() ;			
+    th_hilevel_curr = th_hilevel_prev_ ;
+  }
 
-	unsigned long getlevel()
-   	{return my_hier_ ;}
+  unsigned long getlevel()
+    {return my_hier_ ;}
 
 private:
-	void validate_hier()
- 	{
-		if (my_hier_ >= th_hilevel_curr)
-			throw std::logic_error("Violated mutex hierarchy !") ;			
-	}
+  void validate_hier()
+  {
+    if (my_hier_ >= th_hilevel_curr)
+        throw std::logic_error("Violated mutex hierarchy !") ;			
+  }
 
-	void set_th_hier() 
-	{
-		th_hilevel_prev_ = th_hilevel_curr ;
-		th_hilevel_curr = my_hier_ ;
-	}
+  void set_th_hier() 
+  {
+    th_hilevel_prev_ = th_hilevel_curr ;
+    th_hilevel_curr = my_hier_ ;
+  }
   	
-
 private:
-	static thread_local unsigned long th_hilevel_curr; //hier of the thread acting on this mx			
-	unsigned long        th_hilevel_prev_ ;            //hier of the mx this th holds already before me
+  static thread_local unsigned long th_hilevel_curr;   //hier of the thread acting on this mx			
+  unsigned long                     th_hilevel_prev_ ; //hier of the mx this th holds already before me
 
-	const  unsigned long my_hier_ ;                    //hier of this mx (fixed for its lifetime)
-	std::mutex           mx_  ;
+  const  unsigned long my_hier_ ;                      //hier of this mx (fixed for its lifetime)
+  std::mutex           mx_  ;
 
 };
 thread_local unsigned long mux_hier::th_hilevel_curr = mux_hier::MAXHIER ;
--- mux_hier_tst.cpp
+++ mux_hier_tst.cpp
@@ -6,15 +6,15 @@
 void process( mux_hier& himux  )
 {
 
-	std::cerr << "Requesting level-" << himux.getlevel() << " service" << std::endl ; 
-	try {
-	  himux.lock();
-	  std::cerr << "In level-" << himux.getlevel() << " service" << std::endl ; 
-	}
-	catch (const std::exception& e) {
-		std::cerr << "Refused level-" << himux.getlevel() << " service" << std::endl ;					
-		std::cerr << "error: " << e.what()  << std::endl ;					
-	}
+  std::cerr << "Requesting level-" << himux.getlevel() << " service" << std::endl ; 
+  try {
+    himux.lock();
+    std::cerr << "In level-" << himux.getlevel() << " service" << std::endl ; 
+  }
+  catch (const std::exception& e) {
+    std::cerr << "Refused level-" << himux.getlevel() << " service" << std::endl ;					
+    std::cerr << "error: " << e.what()  << std::endl ;					
+  }
 }
 
 /////////////////////////////////////////////////////////
@@ -24,15 +24,15 @@ int main ()
   mux_hier himux_20{20} ;
   mux_hier himux_10{10} ;
 
-	// this suceeds...
-	std::thread t1(
-      [&]() { process(himux_30); process(himux_20); }
-	) ;			
+  // this suceeds...
+  std::thread t1(
+     [&]() { process(himux_30); process(himux_20); }
+  ) ;			
 
-	// this should fail...
-	std::thread t2(
-      [&]() { process(himux_10); process(himux_20) ;}
-	) ;			
+  // this should fail...
+  std::thread t2(
+     [&]() { process(himux_10); process(himux_20) ;}
+  ) ;			
 
   t2.join() ;
   t1.join() ;	
